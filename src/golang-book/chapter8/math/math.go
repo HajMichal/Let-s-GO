0f25967a3e8ec77ec287a11f2b0347c9a87ab719
HajMichal <michalhaj2003@gmail.com>
2024-07-17 00:02:40 +0200

    New knowledge

--- src/golang-book/chapter8/math/math.go
+++ src/golang-book/chapter8/math/math.go
@@ -1,5 +1,6 @@
 package math
 
+// The good practice of naming functions in go is every function that is visible outside should be wrote with first Capital letter.
 func Average(xs []float64) float64 {
 	total := float64(0)
 	for _, x := range xs {
